6f2034f68e66b2eb149a974237b5244010edbf22
Gleefre <varedif.a.s@gmail.com>
2023-06-04 14:14:31 +0200

    Update config

--- src/config.lisp
+++ src/config.lisp
@@ -1,13 +1,12 @@
 (in-package #:waller)
 
-(defc :default-font "RobotoMono-Bold.ttf")
-(defc :error-font "RobotoMono-Bold.ttf")
+(defc :default-font "RobotoMono-Bold.ttf"
+      :error-font   "RobotoMono-Bold.ttf")
 
-(defc
-  :background  s:+black+
-  :tile        s:+white+
-  :hero        s:+green+
-  :cloud       (s:gray 0.7)
-  :cloud-image "cloud.png"
-  :wall        (s:gray 0.3)
-  :margin 1/10)
+(defc :background  s:+black+
+      :tile        s:+white+
+      :hero        s:+green+
+      :wall        s:+green+
+      :cloud       (s:gray 0.7)
+      :cloud-image "cloud.png"
+      :margin      1/15)
