7d6d030104297b9fdd074f92a530279398e951d2
Gleefre <varedif.a.s@gmail.com>
2023-06-04 19:21:18 +0200

    Add hover / unhover to sketch-buttons

--- src/sketch-buttons.lisp
+++ src/sketch-buttons.lisp
@@ -3,11 +3,14 @@
 (defclass button ()
   ((on-press        :initform NIL :initarg :on-press   :type (or function null) :accessor on-press)
    (on-release      :initform NIL :initarg :on-release :type (or function null) :accessor on-release)
+   (on-hover        :initform NIL :initarg :on-hover   :type (or function null) :accessor on-hover)
+   (on-unhover      :initform NIL :initarg :on-unhover :type (or function null) :accessor on-unhover)
    (%inverse-matrix :initform (sb-cga:inverse-matrix (s::env-model-matrix s::*env*))
                     :accessor %inverse-matrix)))
 
 (defgeneric button-contains (button x y))
 (defgeneric button-event (button state mouse-button x y &rest args &key timestamp))
+(defgeneric button-motion-event (button to-x to-y from-x from-y &rest args &key timestamp button-mask))
 (defgeneric button-press (button mouse-button &rest args &key timestamp))
 (defgeneric button-release (button mouse-button &rest args &key timestamp))
 
@@ -21,16 +24,39 @@
   (alexandria:when-let ((function (on-release button)))
     (funcall function mouse-button)))
 
+(defmethod button-hover ((button button) &key &allow-other-keys)
+  (alexandria:when-let ((function (on-hover button)))
+    (funcall function)))
+
+(defmethod button-unhover ((button button) &key &allow-other-keys)
+  (alexandria:when-let ((function (on-unhover button)))
+    (funcall function)))
+
 (defmethod button-event ((button button) state mouse-button x y &rest args &key &allow-other-keys)
   (when (button-contains button x y)
     (case state
       (:mousebuttondown (apply #'button-press button mouse-button args))
       (:mousebuttonup (apply #'button-release button mouse-button args)))))
 
+(defmethod button-motion-event ((button button) to-x to-y from-x from-y &rest args &key &allow-other-keys)
+  (let ((to (button-contains button to-x to-y))
+        (from (button-contains button from-x from-y)))
+    (when (and to (not from))
+      (apply #'button-hover button args))
+    (when (and (not to) from)
+      (apply #'button-unhover button args))))
+
 (defmethod button-event :around ((button button) state mouse-button x y &rest args &key &allow-other-keys)
   (destructuring-bind (x y)
       (s::transform-vertex (list x y) (%inverse-matrix button))
     (apply #'call-next-method button state mouse-button x y args)))
+
+(defmethod button-motion-event :around ((button button) to-x to-y from-x from-y &rest args &key &allow-other-keys)
+  (destructuring-bind (to-x to-y)
+      (s::transform-vertex (list to-x to-y) (%inverse-matrix button))
+    (destructuring-bind (from-x from-y)
+        (s::transform-vertex (list from-x from-y) (%inverse-matrix button))
+      (apply #'call-next-method button to-x to-y from-x from-y args))))
 
 (defgeneric bind (button type function))
 
@@ -43,6 +69,14 @@
 
 (defmethod bind ((button button) (type (eql :release)) function)
   (setf (on-release button) function)
+  button)
+
+(defmethod bind ((button button) (type (eql :hover)) function)
+  (setf (on-hover button) function)
+  button)
+
+(defmethod bind ((button button) (type (eql :unhover)) function)
+  (setf (on-unhover button) function)
   button)
 
 (defmacro binds (button &body clauses &key &allow-other-keys)
@@ -73,6 +107,11 @@
   (with-buttons ((%buttons app))
     (dolist (button *buttons*)
       (button-event button state mouse-button x y :timestamp timestamp))))
+
+(defmethod kit.sdl2:mousemotion-event :before ((app sketch-buttons) timestamp button-mask x y xrel yrel)
+  (with-buttons ((%buttons app))
+    (dolist (button *buttons*)
+      (button-motion-event button x y (- x xrel) (- y yrel) :timestamp timestamp :button-mask button-mask))))
 
 (defclass rectangle-button (button) ())
 
