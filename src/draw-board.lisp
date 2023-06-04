5a03460d7781e9559b16f5b43ecadf3c64dc3815
Gleefre <varedif.a.s@gmail.com>
2023-06-04 12:02:09 +0200

    Add draw-board

---
+++ src/draw-board.lisp
@@ -0,0 +1,60 @@
+(in-package #:waller)
+
+(defparameter *cell-drawers* (make-hash-table))
+(defparameter *cell-draw-order* '(:tile :uwall :dwall :rwall :lwall :cloud))
+
+(defmacro define-cell-drawer (feature-name (state unit) &body body)
+  `(setf (gethash ,feature-name *cell-drawers*)
+         (lambda (,state ,unit)
+           ,@body)))
+
+(defun call-cell-drawer (cell feature-name unit)
+  (funcall (gethash feature-name *cell-drawers*)
+           (tilep$ cell feature-name)
+           unit))
+
+(defun draw-cell (cell unit)
+  (dolist (feature-name *cell-draw-order*)
+    (s:with-current-matrix
+      (call-cell-drawer cell feature-name unit))))
+
+(define-cell-drawer :tile (state unit)
+  (when state
+    (with-color (:tile)
+      (s:rect (* unit           (c :margin))   (* unit           (c :margin))
+              (* unit (- 1 (* 2 (c :margin)))) (* unit (- 1 (* 2 (c :margin))))))))
+
+(define-cell-drawer :uwall (state unit)
+  (when state
+    (with-color (:wall)
+      (s:rect 0 0 unit (* unit (c :margin))))))
+
+(define-cell-drawer :dwall (state unit)
+  (when state
+    (with-color (:wall)
+      (s:rect 0 (* unit (- 1 (c :margin))) unit (* unit (c :margin))))))
+
+(define-cell-drawer :rwall (state unit)
+  (when state
+    (with-color (:wall)
+      (s:rect (* unit (- 1 (c :margin))) 0 (* unit (c :margin)) unit))))
+
+(define-cell-drawer :lwall (state unit)
+  (when state
+    (with-color (:wall)
+      (s:rect 0 0 (* unit (c :margin)) unit))))
+
+(define-cell-drawer :cloud (state unit)
+  (when state
+    (with-color (:cloud)
+      (s:rect 0 0 unit unit)
+      (s:image (s:load-resource (data-path (c :cloud-image)))
+               0 0 unit unit))))
+
+(defun draw-board (board width height)
+  (with-board (board)
+    (let ((unit (min (/ width (board-width)) (/ height (board-height)))))
+      (do-cells (:cell cell :x x :y y)
+        (s:with-current-matrix
+          (s:translate (* unit x) (* unit y))
+          (draw-cell cell unit))))))
