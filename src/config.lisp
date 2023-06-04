e4c52e124e1446e9996fec4c669b892089300a29
Gleefre <varedif.a.s@gmail.com>
2023-06-04 12:02:22 +0200

    Add more config variables

--- src/config.lisp
+++ src/config.lisp
@@ -2,3 +2,11 @@
 
 (defc :default-font "RobotoMono-Bold.ttf")
 (defc :error-font "RobotoMono-Bold.ttf")
+
+(defc
+  :background  s:+black+
+  :tile        s:+white+
+  :cloud       (s:gray 0.7)
+  :cloud-image "cloud.png"
+  :wall        (s:gray 0.3)
+  :margin 1/10)
